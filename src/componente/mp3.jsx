58ba1e4109d64dacbffa290a4a98a6d7ffaad7c3
Catalina Arroyo <151681207+caadf@users.noreply.github.com>
2024-01-22 21:18:15 +0000

    terminando el molde del proyecto

--- src/componente/mp3.jsx
+++ src/componente/mp3.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { IoPlayBackSharp } from "react-icons/io5"
 import { IoPlaySharp, IoPlayBackSharp, IoPlayForwardSharp } from "react-icons/io5";
 
@@ -7,10 +7,14 @@ import { IoPlaySharp, IoPlayBackSharp, IoPlayForwardSharp } from "react-icons/io
 const Mp3audio = () => {
 
     const [songs, setSongs] = useState([]);
-    const [currentSong, setCurrentSong] = useState("");
+    const [currentsong, setCurrentSong] = useState(null);
     const audioRef = useRef();
 
     useEffect(() => {
+        obtenerCanciones()
+    }, [])
+
+    const obtenerCanciones = () => {
         fetch("https://playground.4geeks.com/apis/fake/sound/songs")
             .then((response) => {
                 console.log(response)
@@ -21,19 +25,37 @@ const Mp3audio = () => {
                 console.log(data)
                 setSongs(data.results)
             })
-    }, [];
+    }
 
     const songUrl = "https://playground.4geeks.com/apis/fake/sound"
 
-    
+    const playcancion = (cancionIndex) => {
+        const cancion = songs[cancionIndex];
+        setCurrentSong(cancionIndex);
+        audioRef.current.src = songUrl + songUrl.url;
+        audioRef.current.play();
+    };
 
+    const stopCancion = () => {
+        audioRef.current.pause();
+    };
+ 
+    const siguienteCancion = () =>{
+        const nextsong = (currentsong + 1) % songs.length;
+        playcancion(nextsong);
+    };
+ 
+    const previaCancion = () => {
+        const lastsong = (currentsong -1) % songs.length;
+        playcancion(lastsong);
+    };
 
     return (
         <>
             <div className="control-players">
-                <button onClick={ }><IoPlayBackSharp /></button>
-                <button onClick={ }><IoPlaySharp /></button>
-                <button onClick={ }><IoPlayForwardSharp /></button>
+                <button onClick={previaCancion }><IoPlayBackSharp /></button>
+                <button onClick={stopCancion }><IoPlaySharp /></button>
+                <button onClick={siguienteCancion }><IoPlayForwardSharp /></button>
             </div>
         </>
     )
